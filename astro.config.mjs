cd20be924f16106b622f55921c4b255ead0bc53a
korigamik <korigamik@gmail.com>
2023-06-06 17:43:53 +0530

    add ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.cadecuddy.com',
+  site: 'https://axiomnsut.github.io',
   integrations: [tailwind({}), mdx(), sitemap(), svelte()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
