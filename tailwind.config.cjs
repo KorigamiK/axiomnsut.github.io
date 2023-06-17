479b334e7950fe2f80c25c65cb808e9cbc005439
korigamik-hypr <korigamik@gmail.com>
2023-06-18 01:58:07 +0530

    new font styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Segoe Ui', 'sans-serif'],
+				sans: ['Open Sans', 'sans-serif', ...defaultTheme.fontFamily.sans],
+				serif: ['Josefin Sans', ...defaultTheme.fontFamily.serif],
 			},
 		},
 	},
